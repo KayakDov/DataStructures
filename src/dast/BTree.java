489e9bdc7bd6cf9ee2d9f2d8d0772e3e79f62c95
Dov Neimand <KayakDov@gmail.com>
2023-12-05 19:38:18 +0200

    B tree delete has bugs.

--- src/dast/BTree.java
+++ src/dast/BTree.java
@@ -1,7 +1,8 @@
 package dast;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
-import tools.SplitArray;
+import tools.MyArray;
 
 /**
  * B Tree Node
@@ -12,8 +13,12 @@ import tools.SplitArray;
 public class BTree<T extends Comparable<T>> {
 
     private final T[] keys;
-    private final BTree<T>[] children;
+    private BTree<T>[] children;
     private int numKeys;
+
+    public int getNumKeys() {
+        return numKeys;
+    }
 
     /**
      * The constructor
@@ -25,8 +30,9 @@ public class BTree<T extends Comparable<T>> {
      * BTree behavior.
      */
     public BTree(T... keys) {
+        if (keys == null)
+            throw new IllegalArgumentException("keys must not be null");
         this.keys = keys;
-        children = new BTree[keys.length + 1];
         numKeys = countKeys();
     }
 
@@ -37,7 +43,8 @@ public class BTree<T extends Comparable<T>> {
      */
     private int countKeys() {
         int i = 0;
-        while (i < keys.length && keys[i] != null) i++;
+        while (i < keys.length && keys[i] != null)
+            i++;
         return i;
     }
 
@@ -47,7 +54,7 @@ public class BTree<T extends Comparable<T>> {
      * @return True if this node is a leaf, false otherwise.
      */
     public boolean isLeaf() {
-        return children == null;
+        return children == null || children[0] == null;
     }
 
     /**
@@ -87,63 +94,308 @@ public class BTree<T extends Comparable<T>> {
      */
     private KeyLocation search(T key) {
         int index = indexOf(key);
-        if (index >= 0)  return new KeyLocation(this, index);
-        if (!isLeaf()) return children[-index - 1].search(key);
-        else return null;
+        if (index >= 0)
+            return new KeyLocation(this, index);
+        if (!isLeaf())
+            return children[-index - 1].search(key);
+        else return new KeyLocation(null, -1);
+    }
+
+    /**
+     * Is this node full? Can a new key be inserted here?
+     *
+     * @return True if this node is full, false otherwise.
+     */
+    private boolean isFull() {
+        return getNumKeys() == keys.length;
+    }
+
+    /**
+     * Inserts all the keys provided.
+     *
+     * @param keys The keys to be inserted to the tree.
+     * @return The new root of the tree.
+     */
+    public BTree<T> insert(T... keys) {
+        BTree<T> root = this;
+        for (T key : keys)
+            root = root.insert(key);
+        return root;
+    }
+
+    /**
+     * Creates a parent node for this node, then splits this node.
+     *
+     * @return The parent node.
+     */
+    private BTree<T> splitRoot() {
+        BTree<T> root = new BTree<>(keys[0].getClass(), keys.length);
+        root.openChildren();
+        root.children[0] = this;
+        root.splitChild(0);
+        return root;
+    }
+
+    /**
+     * Finds where this key would be inserted. If this is a leaf, inserts a key
+     * into this node's keys.
+     *
+     * @param key The key to be inserted.
+     * @return The index of where the insertion would be.
+     */
+    private int shiftToIndex(T key) {
+        int i;
+        for (i = getNumKeys(); i > 0 && keys[i - 1].compareTo(key) > 0; i--)
+            if (isLeaf()) keys[i] = keys[i - 1];
+        return i;
+    }
+
+    /**
+     * Inserts the key to the child with the proffered index.
+     *
+     * @param key The key to be inserted.
+     * @param childIndex The index of the child to recieve the key.
+     * @return The root node of the entire tree.
+     */
+    private void insertInChild(T key, int childIndex) {
+
+        if (children[childIndex].isFull()) {
+            splitChild(childIndex);
+            if (keys[childIndex].compareTo(key) < 0) {
+                children[childIndex + 1].insert(key);
+                return;
+            }
+        }
+        children[childIndex].insert(key);
     }
 
     /**
      * Inserts the proffered key into this tree.
      *
      * @param key The key to be inserted.
+     * @return The root of the tree after the insert.
      */
-    private void insert(T key) {
-        if (numKeys < keys.length) {
-            int i = numKeys - 1;
-            while (i > 0 && keys[i].compareTo(key) > 0) {
-                keys[i] = keys[i + 1];
-                i--;
-            }
-            keys[i] = key;
-
+    public BTree<T> insert(T key) {
+        if (getNumKeys() >= keys.length) return splitRoot().insert(key);
+        if (getNumKeys() == 0) {
+            keys[0] = key;
+            numKeys++;
+            return this;
         }
 
+        if (isLeaf()) {
+            keys[shiftToIndex(key)] = key;
+            numKeys++;
+        } else insertInChild(key, indexOf(key));
+
+        return this;
     }
 
     /**
      * Does a binary search for the desired element.
-     * @param key The key whos index is desired.
+     *
+     * @param key The key whose index is desired.
      * @return Same as Arrays.binarySearch, except null elements are ignored.
      */
-    private int indexOf(T key){
-        return Arrays.binarySearch(keys, 0, numKeys, key);
-    }
-    
-    /**
-     * Deletes a key from this node.
-     *
-     * @param key The key to be deleted.
-     */
-    private void delete(T key) {
-        int i = indexOf(key);
-        if (i >= 0) {
-            System.arraycopy(keys, i + 1, keys, i, numKeys - i - 1);
-            numKeys--;
-        }
-
+    private int indexOf(T key) {
+        return Arrays.binarySearch(keys, 0, getNumKeys(), key);
     }
 
     /**
      * A constructor
-     * @param keys The keys of this node.
-     * @param children The children of the keys of this node.
+     *
+     * @param componentType the type of elements in the tree.
+     * @param length The number of keys in each node.
      */
-    private BTree(T[] keys, BTree<T>[] children) {
-        this.keys = keys;
-        this.children = children;
-        numKeys = countKeys();
+    private BTree(Class<?> componentType, int length) {
+        this((T[]) Array.newInstance(componentType, length));
     }
-    
+
+    /**
+     * Opens up the children. Should not be called if children is not null.
+     */
+    private void openChildren() {
+        children = (BTree<T>[]) Array.newInstance(getClass(),
+                keys.length + 1);
+    }
+
+    /**
+     * merges children at indicis i and i + 1, and moves key[i] into that set of
+     * children.
+     *
+     * @param i The index of the first child to be merged.
+     */
+    private BTree<T> mergeChild(int i) {
+        BTree<T> mergeChild = new BTree<>(keys[0].getClass(), keys.length);
+        BTree<T> left = children[i], right = children[i + 1];
+        System.arraycopy(left.keys, 0, mergeChild.keys, 0, left.getNumKeys());
+        mergeChild.keys[left.getNumKeys()] = keys[i];
+        System.arraycopy(right.keys, 0, mergeChild.keys, left.getNumKeys() + 1,
+                right.getNumKeys());
+        mergeChild.numKeys = left.numKeys + right.numKeys + 1;
+        MyArray.delete(i, getNumKeys(), keys);
+        MyArray.delete(i + 1, getNumKeys() + 1, children);
+        children[i] = mergeChild;
+        if (!left.isLeaf()) {
+            mergeChild.openChildren();
+            System.arraycopy(left.children, 0, mergeChild.children, 0,
+                    left.getNumKeys() + 1);
+            System.arraycopy(right.children, 0, mergeChild.children,
+                    left.getNumKeys() + 1, right.getNumKeys() + 1);
+        }
+        numKeys--;
+        return mergeChild;
+    }
+
+    /**
+     * Does the child at childIndex have enough keys to give one away?
+     *
+     * @param childIndex The index of the child in question.
+     * @return True if the child has enough keys to give one away, false
+     * otherwise.
+     */
+    private boolean hasKeyToGive(int childIndex) {
+        return childIndex >= 0 && childIndex < children.length
+                && children[childIndex].getNumKeys() > keys.length / 2;
+    }
+
+    /**
+     * Deltes the first key and its left child.
+     */
+    private void deleteFirst() {
+        MyArray.delete(0, numKeys, keys);
+        MyArray.delete(0, numKeys + 1, children);
+        numKeys--;
+    }
+
+    /**
+     * Deletes the last child.
+     */
+    private void deleteLast() {
+        keys[getNumKeys() - 1] = null;
+        children[getNumKeys()] = null;
+        numKeys--;
+    }
+
+    /**
+     * Inserts a key and its child to the begining of this array.
+     *
+     * @param key The key to be inserted.
+     * @param child A tree of elements less than the key.
+     */
+    private void insertFirst(T key, BTree<T> child) {
+        MyArray.insert(0, numKeys, key, keys);
+        MyArray.insert(0, numKeys + 1, child, children);
+        numKeys++;
+    }
+
+    /**
+     * Appends a key and its children to the end of this array.
+     *
+     * @param key The key to be appended.
+     * @param child A subtree of elements greater than the appended key.
+     */
+    private void insertLast(T key, BTree<T> child) {
+        keys[numKeys] = key;
+        children[numKeys + 1] = child;
+        numKeys++;
+    }
+
+    /**
+     * Moves the min/max key from the right/left child up to index i / i - 1,
+     * and moves the key at index i down to the ith child.
+     *
+     * @param index The index of the child in need of a key
+     * @param left Is the donator on the left (true), or the right (false).
+     */
+    private void rotateKey(int index, boolean left) {
+        BTree<T> reciever = children[index],
+                donator = children[index + (left ? -1 : 1)];
+        int keyIndex = left ? index - 1 : index;
+        BTree<T> grandChGift = donator.isLeaf()? null: donator.children[left ? donator.numKeys + 1 : 0];
+
+        if (left) {
+            donator.deleteLast();
+            reciever.insertFirst(keys[keyIndex], grandChGift);
+        } else {
+            donator.deleteFirst();
+            reciever.insertLast(keys[keyIndex], grandChGift);
+        }
+        keys[keyIndex] = donator.keys[left ? donator.getNumKeys() : 0];
+    }
+
+    /**
+     * To delete a key that's not present in this node's list of keys.
+     *
+     * @param key The key to be deleted.
+     * @param i The index of the child that should contain the key.
+     * @return The root of this tree.
+     */
+    private BTree<T> deleteKeyNotHere(int i, T key) {
+        if (isLeaf()) return this;
+        i = -i - 1;
+        if (children[i].getNumKeys() <= keys.length / 2) { //do 3a or 3b and then call delte on child.
+            if (hasKeyToGive(i + 1)) rotateKey(i, false);
+            else if (hasKeyToGive(i - 1)) rotateKey(i, true);
+            else mergeChild(i == getNumKeys() ? i - 1 : i);
+        }
+        children[i].delete(key);
+        return numKeys > 0 ? this : children[0];
+    }
+
+    /**
+     * Deletes a key from an internal node.
+     *
+     * @param i The index of the key to be delted.
+     */
+    private void deleteInternalNodeKey(int i) {
+        if (children[i].keys.length >= children.length / 2) {
+            T predecssor
+                    = children[i].keys[children[i].getNumKeys() - 1];
+            children[i].delete(predecssor);
+            keys[i] = predecssor;
+        } else if (children[i + 1].keys.length >= children.length / 2) {
+            T successor = children[i + 1].keys[0];
+            children[i + 1].delete(successor);
+            keys[i] = successor;
+        } else {
+            T key = keys[i];
+            mergeChild(i).delete(key);
+        }
+    }
+
+    /**
+     * Deletes a key from this subtree.
+     *
+     * @param key The key to be deleted.
+     * @return The root of the tree.
+     */
+    private BTree<T> delete(T key) {
+        int i = indexOf(key);
+        if (i < 0) return deleteKeyNotHere(i, key);
+        if (isLeaf()) {
+            MyArray.delete(i, getNumKeys(), keys);
+            numKeys--;
+        } else deleteInternalNodeKey(i);
+
+        return this;
+    }
+
+    /**
+     * Splits the grandchildren around the median grandchild of the given child
+     * index.
+     *
+     * @param childIndex The index of the child whose grandchildren need to be
+     * split.
+     */
+    private void splitGrandChildren(int childIndex) {
+        BTree<T> child = children[childIndex];
+        MyArray<BTree<T>> grandChildKeys = MyArray.split(
+                child.children, (child.getNumKeys() + 1) / 2);
+        children[childIndex].children = grandChildKeys.left;
+        children[childIndex + 1].children = grandChildKeys.right;
+    }
+
     /**
      * Splits this child at the given index, pulling the median element of the
      * child up into this node.
@@ -151,74 +403,98 @@ public class BTree<T extends Comparable<T>> {
      * @param childIndex The index of child to be split.
      */
     private void splitChild(int childIndex) {
-        
+
         BTree<T> child = children[childIndex];
-        SplitArray<T> childKeys = 
-                SplitArray.around(child.keys, child.numKeys/2);
-        SplitArray<BTree<T>> grandChildKeys = 
-                SplitArray.halfs(child.children, (child.numKeys + 1)/2);
+        MyArray<T> childKeys = MyArray.splitAround(
+                child.keys, child.getNumKeys() / 2);
+
+        MyArray.insert(
+                childIndex, 
+                numKeys++, 
+                child.keys[child.getNumKeys()/ 2], 
+                keys);
+        MyArray.insert(
+                childIndex, 
+                getNumKeys(), 
+                new BTree<>(childKeys.left),
+                children);
         
-        BTree<T> leftOfMid = 
-                new BTree<>(childKeys.left, grandChildKeys.left), 
-                rightOfMid = 
-                new BTree<>(childKeys.right, grandChildKeys.right);
-        
-        for(int i = numKeys++; i > childIndex; i--){
-            keys[i] = keys[i - 1];
-            children[i + 1] = children[i];
-        }
-        
-        keys[childIndex] = child.keys[child.numKeys/2];
-        children[childIndex] = leftOfMid;
-        children[childIndex + 1] = rightOfMid;
+        children[childIndex + 1] = new BTree<>(childKeys.right);
+
+        if (child.children != null) splitGrandChildren(childIndex);
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = 
-                new StringBuilder(Arrays.toString(keys)).append("\n");
-        for(BTree<T> child: children) if(child != null) 
-            sb.append(Arrays.toString(child.keys)).append(" ");
+        StringBuilder sb
+                = new StringBuilder(Arrays.toString(keys)).append("\n");
+        if (children != null)
+            for (BTree child : children)
+                if (child != null)
+                    sb.append(Arrays.toString(child.keys)).append(" ");
         return sb.toString();
     }
-    
+
     /**
      * A sample tree for testing
-     * 
+     *
      * @return A sample tree for testing
      */
-    private static BTree<Integer> sampleTreeForTesting(){
+    private static BTree<Integer> sampleTreeForTesting() {
         BTree<Integer> testTree = new BTree<>(4, 8, 16, null);
         testTree.children[0] = new BTree<>(1, 2, 3, null);
         testTree.children[1] = new BTree<>(5, 6, 7, null);
         testTree.children[2] = new BTree<>(10, 12, 14, null);
         testTree.children[3] = new BTree<>(17, 18, 19, null);
-        
-        testTree.children[2].children[0] =  
-                new BTree<>(9, null, null, null);
-        testTree.children[2].children[1] =  
-                new BTree<>(11, null, null, null);
-        testTree.children[2].children[2] =  
-                new BTree<>(13, null, null, null);
-        testTree.children[2].children[3] =  
-                new BTree<>(15, null, null, null);
-        
+
+        testTree.children[2].children[0]
+                = new BTree<>(9, null, null, null);
+        testTree.children[2].children[1]
+                = new BTree<>(11, null, null, null);
+        testTree.children[2].children[2]
+                = new BTree<>(13, null, null, null);
+        testTree.children[2].children[3]
+                = new BTree<>(15, null, null, null);
+
         return testTree;
     }
-    
+
     /**
-     * For testing.
-     * @param args 
+     * Tests the splitChild method.
      */
-    public static void main(String[] args) {
-        
+    public static void testSplit() {
+
         BTree<Integer> testTree = sampleTreeForTesting();
-        
+
         testTree.splitChild(2);
-        
+
         System.out.println(testTree);//4, 8, 12, 16 \n 123 567 10 14 171819
         System.out.println(testTree.children[2]);//10 \n 9 11
         System.out.println(testTree.children[3]);//14 \n 13 15
+    }
 
+    /**
+     * Tests the insert function.
+     */
+    private static void test() {
+        BTree<Integer> testTree = new BTree<>(null, null, null);
+
+        testTree = testTree.insert(4, 30, 15, 20, 25);
+        
+        testTree = testTree.delete(4);
+        
+        System.out.println(Arrays.toString(testTree.keys));
+        System.out.println();
+        System.out.println(Arrays.toString(testTree.children));
+    }
+    
+
+    /**
+     * For testing.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        test();
     }
 }
--- src/tools/MyArray.java
+++ src/tools/MyArray.java
@@ -1,5 +1,6 @@
 package tools;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -7,7 +8,7 @@ import java.util.Arrays;
  * @author Dov Neimand
  * @param <T> The type of element in the array.
  */
-public class SplitArray<T> {
+public class MyArray<T> {
     /**
      * The first half of the proffered array.
      */
@@ -22,7 +23,7 @@ public class SplitArray<T> {
      * @param left The first half of the array.
      * @param right The second half of the array.
      */
-    private SplitArray(T[] left, T[] right) {
+    private MyArray(T[] left, T[] right) {
         this.left = left;
         this.right = right;
     }
@@ -36,13 +37,12 @@ public class SplitArray<T> {
      * @param src The array from which the sub array is taken.
      * @param start The starting index of the sub array, inclusive.
      * @param end The end index of the sub array exclusive.
-     * @return A new array with the same length as src, but with all traling 
-     * values of the sub array set to null.
+     * @return A new array with the same length as src, but with all trailing 
+     * values of the sub array set to null. 
      */
     private static <T> T[] createSub(T[] src, int start, int length){
-        T[] sub = Arrays.copyOf(src, src.length);
-        if(start != 0) 
-            System.arraycopy(src, start, sub, 0, length);
+        T[] sub = (T[])Array.newInstance(src[0].getClass(), src.length);
+        System.arraycopy(src, start, sub, 0, length);
         Arrays.fill(sub, length, sub.length, null);
         return sub;
     }
@@ -55,12 +55,14 @@ public class SplitArray<T> {
      * a copy of the second half of needsSplitting.  Both arrays will be the
      * same length as needsSpllitng with their 2nd half's null.
      * @param splitAt The index the array is split at
-     * @return A SplitArray that is src split in two arrays.
+     * @return A SplitArray that is src split in two arrays.  If src is null
+     * then the split of src will be two null arrays.
      */
-    public static <T> SplitArray<T> halfs(T[] src, int splitAt) {
+    public static <T> MyArray<T> split(T[] src, int splitAt) {
+        
         int leftLength = splitAt,
                 rightLength = src.length - leftLength;
-        return new SplitArray<>(
+        return new MyArray<>(
                 createSub(src, 0, leftLength), 
                 createSub(src, leftLength, rightLength)
         );
@@ -74,10 +76,10 @@ public class SplitArray<T> {
      * @param removeInd the index to be removed, around which the array is split.
      * @return A Split Array without the middle element from src.
      */
-    public static <T> SplitArray<T> around(T[] src, int removeInd){
+    public static <T> MyArray<T> splitAround(T[] src, int removeInd){
         int leftLength = removeInd,
                 rightLength = src.length - leftLength - 1;
-        return new SplitArray<>(
+        return new MyArray<>(
                 createSub(src, 0, leftLength), 
                 createSub(src, leftLength + 1, rightLength)
         );
@@ -88,17 +90,47 @@ public class SplitArray<T> {
         return Arrays.toString(left) + "\n" + Arrays.toString(right);
     }
     
+    
+    /**
+     * Deletes the ith element from the proffered array
+     *
+     * @param <K> The contents of the array.
+     * @param i The index whos element is to be removed.
+     * @param lastIndex the number of elements in the array. All elements from
+     * the last index are null.
+     * @param array The array from whom the ith element is to be deleted.
+     */
+    public static <K> void delete(int i, int lastIndex, K[] array) {
+        System.arraycopy(array, i + 1, array, i, lastIndex - i - 1);
+
+    }
+    
+    /**
+     * Inserts a value into the given array.
+     * @param <K> The type of the array.
+     * @param index The index the element is to be inserted to.
+     * @param lastIndex The index of the last element of the array.  After this
+     * all elements should be null.
+     * @param val The value to be inserted at the given index.
+     * @param array The array to receive the insertion.
+     */
+    public static <K> void insert(int index, int lastIndex, K val, K[] array){
+        for(int i = lastIndex; i >= index; i--)
+            array[i + 1] = array[i];
+        array[index] = val;
+    }
+    
     /**
      * Tests this class.
      * @param args 
      */
     public static void main(String[] args) {
         Character[] testArray = new Character[]{'a', 'b', 'c'};
-        System.out.println(SplitArray.halfs(testArray, 1));
-        System.out.println(SplitArray.around(testArray, 1));
+        System.out.println(MyArray.split(testArray, 1));
+        System.out.println(MyArray.splitAround(testArray, 1));
         
         testArray = new Character[]{'a', 'b', 'c', 'd', 'e', 'f'};
-        System.out.println(SplitArray.halfs(testArray, 3));
-        System.out.println(SplitArray.around(testArray, 3));
+        System.out.println(MyArray.split(testArray, 3));
+        System.out.println(MyArray.splitAround(testArray, 3));
     }
 }
